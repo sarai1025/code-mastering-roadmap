0ec9802856cf7473de1bb982e86962f9f4e5eca8
sarai romero <sarairomero1025@hotmail.com>
2025-08-23 22:46:48 -0500

    Init repo, folder structure and 2 leetCodeProblems

---
+++ CSharp/CodeInterview/LeetCode/LeetCodeTest/Easy/ContainsDuplicatesTest.cs
@@ -0,0 +1,23 @@
+﻿using LeetCodeProblems.Easy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTest.Easy
+{
+    public  class ContainsDuplicatesTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            ContainsDuplicates containsDuplicates = new ContainsDuplicates();
+            int[] nums = [1, 2, 3, 1];
+
+            var result = containsDuplicates.ContainsDuplicateMostOptimized(nums);
+
+            Assert.True(result);
+        }
+    }
+}
