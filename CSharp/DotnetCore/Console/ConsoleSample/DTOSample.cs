cfaaacba1a549bb0a8520f69840c837eb43e574f
sarai romero <sarairomero1025@hotmail.com>
2025-08-26 19:37:35 -0500

    Console project with dotnet core to compare the program.cs styles and the implicit and global usings

---
+++ CSharp/DotnetCore/Console/ConsoleSample/DTOSample.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleSample
+{
+    internal class DTOSample
+    {
+        public int Id { get; set; }
+        public int Ad { get; set; }
+
+
+        //This is a method to validate how the explicit global using works
+        //To achive the operation in line 16 is required the using System.Text.Json,
+        //However, that using is not in this current file. 
+        //
+        //It works because System.Text.Json was included as a global using in .csproj file
+        public static string ExplicitUsing(string text)
+        {
+            return JsonSerializer.Serialize(text);
+        }
+    }
+}
---
+++ CSharp/DotnetCore/Console/ConsoleSample/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
+
+//
+//
+//The previous code is the same as follows, the only difference is the syle.
+//
+//
+//namespace ConsoleSample
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("What is your name?");
+//            var name = Console.ReadLine();
+//            var currentDate = DateTime.Now;
+//            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+//            Console.Write($"{Environment.NewLine}Press any key to exit...");
+//            Console.ReadKey(true);
+//        }
+//    }
+//}
