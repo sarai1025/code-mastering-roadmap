ebc2288d7d4fba1c9faf847f484f6721dc6a0afa
sarai romero <sarairomero1025@hotmail.com>
2025-09-10 21:00:26 -0500

    asbtract class example

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/CommonTypesProject/Classes/AbstractClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypesProject.Classes
+{
+    public abstract class AbstractClass1
+    {
+        public abstract void Method1(); //abstracts methods should be declare within abstract class
+                                        //Methods not implemented must have declared as abstract, extern or partial
+
+        public string Method2() { return string.Empty; }
+    }
+
+    public abstract class AbstractClass2 { }
+
+
+
+    public class ConcretClass1 : AbstractClass1 //, AbstractClass2 (error)
+    {
+        public override void Method1() //abstract methods must be override 
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ConcretClass2
+    {
+        public void Method()
+        {
+            var a = new ConcretClass1 { };
+            a.Method1();
+            a.Method2();
+        }
+    }
+}
