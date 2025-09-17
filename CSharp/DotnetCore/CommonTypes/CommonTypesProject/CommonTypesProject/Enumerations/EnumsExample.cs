6a2aed9715f4b5a71bf6473d2c862300728ebde6
sarai romero <sarairomero1025@hotmail.com>
2025-09-16 19:20:48 -0500

    Enums practice

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/CommonTypesProject/Enumerations/EnumsExample.cs
@@ -0,0 +1,24 @@
+﻿namespace CommonTypesProject.Enumerations
+{
+    enum Days
+    {
+        Monday = 1,
+        Tuesday = 2,
+        Wednesday = 3,
+        Thursday = 4,
+        Friday = 5,
+        Saturday = 6,
+        Sunday = 7
+    }
+
+
+    public class EnumsExample()
+    {
+        public static void AccessToEnum()
+        {
+            Console.WriteLine($"Today is {(Days)DateTime.Now.DayOfWeek}");
+            Console.WriteLine($"Weeks starts on {Days.Monday}, that means this day is the number {(int)Days.Monday} in the week.");
+            Console.ReadKey();
+        }
+    }
+}
--- CSharp/DotnetCore/CommonTypes/CommonTypesProject/CommonTypesProject/Program.cs
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/CommonTypesProject/Program.cs
@@ -1,7 +1,10 @@
 ﻿using BenchmarkDotNet.Running;
 using CommonTypesProject.Classes;
+using CommonTypesProject.Enumerations;
 using CommonTypesProject.Structures;
 
 BenchmarkRunner.Run<BenchmarkSealedClass>();
 
 StructTest.Compare();
+
+EnumsExample.AccessToEnum();
