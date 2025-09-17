bb01e2087d008ea8d630771cda4ad99c736d6703
sarai romero <sarairomero1025@hotmail.com>
2025-09-16 20:05:51 -0500

    interfaces practice

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/CommonTypesProject/Interfaces/InterfaceExample.cs
@@ -0,0 +1,38 @@
+﻿namespace CommonTypesProject.Interfaces
+{
+    interface InterfaceExample
+    {
+        public string property { get; set; }
+
+        //public string Name; // (error) fields are not allowed in interfaces
+        //public InterfaceExample() { } //(error) constructors are not allowed in interfaces
+
+        void Method();
+
+        event EventHandler EventH;
+    }
+
+    public interface  Interface2
+    {
+        //private string property { get; set; } //An interface can not be private or protected, neither its declaratios nor its members
+        void Method2() { }
+        void Method3();
+    }
+
+    public class ConcretClass : InterfaceExample, Interface2
+    {
+        public string property { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event EventHandler EventH;
+
+        public void Method()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Method3()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
