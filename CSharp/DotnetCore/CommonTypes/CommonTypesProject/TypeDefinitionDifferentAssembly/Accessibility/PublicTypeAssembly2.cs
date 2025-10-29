8c89b64c34be03bbb018afbf7cee9dbdbd6b3b14
sarai romero <sarairomero1025@hotmail.com>
2025-10-28 20:26:17 -0500

    Testing different access types -> public

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionDifferentAssembly/Accessibility/PublicTypeAssembly2.cs
@@ -0,0 +1,27 @@
+﻿using TypeDefinitionProject.Accessibility;
+
+namespace TypeDefinitionAssembly2Project.Accessibility
+{
+    public class PublicClassDifferentAssembly
+    {
+        public void Method()
+        {
+            PublicType publicType = new PublicType();
+            var value = publicType.PublicProperty;
+            publicType.PublicMethod();
+            
+            Console.WriteLine("Access to public property and method from different class and different assembly: Valid");
+        }
+    }
+
+    public class PublicDerivedClassDifferentAssembly : PublicType
+    {
+        public void Method()
+        {
+            var value = PublicProperty;
+            PublicMethod();
+
+            Console.WriteLine("Access to public property and method from a derived class of a different assembly: Valid");
+        }
+    }
+}
---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionProject/Accessibility/PublicType.cs
@@ -0,0 +1,35 @@
+﻿
+namespace TypeDefinitionProject.Accessibility
+{
+    public class PublicType
+    {
+        public int PublicProperty { get; set; }
+        public void PublicMethod()
+        {
+            PublicProperty = 1;
+        }
+    }
+
+    public class PublicClassSameAssembly
+    { 
+        public void Method()
+        {
+            PublicType publicType = new PublicType();
+            var value = publicType.PublicProperty;
+            publicType.PublicMethod();
+
+            Console.WriteLine("Access to public property and method from different class into the same assembly: Valid");
+        }
+    }
+
+    public class PublicDerivedClassSameAssembly : PublicType 
+    {
+        public void Method()
+        {
+            var value = PublicProperty;
+            PublicMethod();
+
+            Console.WriteLine("Access to public property and method from derived class into the same assembly: Valid");
+        }
+    }
+}
