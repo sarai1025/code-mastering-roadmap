90e44fb2df674713b914c51434f2ad1bf9a0fdbd
sarai romero <sarairomero1025@hotmail.com>
2026-02-03 19:25:54 -0500

    Test to try to access to an internal class from a different assembly

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionDifferentAssembly/Accessibility/InternalTypeAssembly2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeDefinitionAssembly2Project.Accessibility
+{
+    public class InternalClassDifferentAssembly
+    {
+        private void Method()
+        {
+            //Internal class can not be instanciated from an external assembly.
+            //var internalClass = new InternalClass();
+        }
+    }
+
+    //Internal class can not be derived from an external assembly.
+    //public class InternalClassDerivedAssembly : InternalType
+    //{
+
+    //}
+}
