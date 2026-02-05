8b7906acc6919a9f1776fb4abc8fd586238017ea
sarai romero <sarairomero1025@hotmail.com>
2026-02-04 20:50:35 -0500

    Testing protected level access

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionDifferentAssembly/Accessibility/Protected/ProtectedTypeAssembly2.cs
@@ -0,0 +1,23 @@
+﻿using TypeDefinitionProject.Accessibility.Protected;
+
+namespace TypeDefinitionAssembly2Project.Accessibility.Protected
+{
+    public class ProtectedTypeAssembly2
+    {
+        public void Method()
+        {
+            var protectedType = new ProtectedType();
+
+            //Can not access to protected level because it is instantiated (same or different assembly), only derived (same or different assembly) or itself class can do.
+            //var canAccessToProtectedLevel = protectedType.ProtectedProperty && ProtectedProperty.ProtectedMethod();
+        }
+    }
+
+    public class ProtectedTypeDerivedAssembly2 : ProtectedType
+    {
+        public void Method()
+        {
+            var canAccessToProtectedLevel = ProtectedProperty && ProtectedMethod();
+        }
+    }
+}
---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionProject/Accessibility/Protected/ProtectedType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeDefinitionProject.Accessibility.Protected
+{
+    //this class can not be protected, the only access allowed is public and internal
+    public class ProtectedType
+    {
+        protected bool ProtectedProperty { get; set; }
+        protected bool ProtectedMethod() { return ProtectedProperty; }
+    }
+
+    public class ProtectedTypeSameAssebly
+    {
+        public void Method()
+        {
+            var protectedType = new ProtectedType();
+
+            //Can not access to protected level because it is instantiated, only derived or itself class can do.
+            //var canAccessToProtectedLevel = protectedType.ProtectedProperty && ProtectedProperty.ProtectedMethod();
+        }
+    }
+
+    public class ProtectedTypeDerivedSameAssembly : ProtectedType
+    {
+        public void Method()
+        {
+            var canAccessToProtectedLevel = ProtectedProperty && ProtectedMethod();
+        }
+    }
+}
