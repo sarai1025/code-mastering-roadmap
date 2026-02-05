62237988fbe736ec2a4311b0bc0a1bfb3bdcca10
sarai romero <sarairomero1025@hotmail.com>
2026-02-04 20:56:32 -0500

    testing private protected access level

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionDifferentAssembly/Accessibility/Protected/PrivateProtectedTypeAssembly2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeDefinitionProject.Accessibility.Protected;
+
+namespace TypeDefinitionAssembly2Project.Accessibility.Protected
+{
+    public class PrivateProtectedTypeAssembly2
+    {
+        public void Method()
+        {
+            var privateProtectedType = new PrivateProtectedType();
+            //Private protected can not access through instantiation in a different assembly
+            //var canAccessToPrivateProtectedLevel = privateProtectedType.PrivateProtectedProperty && privateProtectedType.PrivateProtectedMethod();
+        }
+    }
+
+    public class PrivateProtectedDifferentAssembly : PrivateProtectedType
+    {
+        public void Method()
+        {
+            //Despite the protected type can be accessed through derived classes, if the protected type is private, it is not possible to accessed through different assembly
+            //var canAccessToPrivateProtectedLevel = PrivateProtectedProperty && PrivateProtectedMethod();
+        }
+    }
+}
---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionProject/Accessibility/Protected/PrivateProtectedType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeDefinitionProject.Accessibility.Protected
+{
+    public class PrivateProtectedType
+    {
+        private protected bool PrivateProtectedProperty {  get; set; }
+        private protected bool PrivateProtectedMethod() {  return PrivateProtectedProperty; }
+    }
+
+    public class PrivateProtectedSameAssembly
+    {
+        public void Method()
+        {
+            var privateProtectedType = new PrivateProtectedType();
+            //Private protected can not access through instantiation despite it is the same assembly
+            //var canAccessToPrivateProtectedLevel = privateProtectedType.PrivateProtectedProperty && privateProtectedType.PrivateProtectedMethod();
+        }
+    }
+
+    public class PrivateProtectedDifferentAssembly : PrivateProtectedType
+    {
+        public void Method()
+        {
+            var canAccessToPrivateProtectedLevel = PrivateProtectedProperty && PrivateProtectedMethod();
+        }
+    }
+}
