5c7860d3f2b00909dff2f5913d09c2b2c742bea1
sarai romero <sarairomero1025@hotmail.com>
2026-02-05 20:09:21 -0500

    testing protected internal access

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionDifferentAssembly/Accessibility/Protected/ProtectedInternalTypeAssembly2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeDefinitionProject.Accessibility.Protected;
+
+namespace TypeDefinitionAssembly2Project.Accessibility.Protected
+{
+    public class ProtectedInternalTypeAssembly2
+    {
+        public void Method()
+        {
+            var protectedInternalType = new ProtectedInternalType();
+            //The only level that a protected internal is not allowed is for an instantiation in a different assembly
+            //var canAccessToProtectedInternalLevel = protectedInternalType.ProtectedInternalProperty && protectedInternalType.ProtectedInternalMethod();
+        }
+    }
+
+    public class ProtectedInternalDerivedDifferentAssembly : ProtectedInternalType
+    {
+        public void Method()
+        {
+            var canAccessToProtectedInternalLevel = ProtectedInternalProperty && ProtectedInternalMethod();
+        }
+    }
+
+
+}
---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeDefinitionProject/Accessibility/Protected/ProtectedInternalType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeDefinitionProject.Accessibility.Protected
+{
+    public class ProtectedInternalType
+    {
+        protected internal bool ProtectedInternalProperty { get; set; }
+        protected internal bool ProtectedInternalMethod() { return ProtectedInternalProperty; }
+    }
+
+    public class ProtectedInternalSameAssembly
+    {
+        public void Method()
+        {
+            var protectedInternalType = new ProtectedInternalType();
+            var canAccessToProtectedInternalLevel = protectedInternalType.ProtectedInternalProperty && protectedInternalType.ProtectedInternalMethod();
+        }
+    }
+
+    public class ProtectedInternalDerivedSameAssembly : ProtectedInternalType
+    {
+        public void Method()
+        {
+            var canAccessToProtectedInternalLevel = ProtectedInternalProperty && ProtectedInternalMethod();
+        }
+    }
+}
