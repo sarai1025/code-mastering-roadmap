ae1fa6fb906d84346459977daf3cdbd69a0e2a6b
sarai romero <sarairomero1025@hotmail.com>
2026-02-11 20:14:08 -0500

    Example of fields type member

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeMembers/Fields.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeMembers
+{
+    public class Fields
+    {
+        public const double Pi = 3.1416;
+        public readonly string Name;
+        private bool Available;
+
+        public Fields(string name)
+        {
+            Name = name;
+            Available = true;
+        }
+
+    }
+
+    public class  FieldsExample
+    {
+        public static void Main()
+        {
+            Fields FieldClass = new("Radius");
+
+            Console.WriteLine(Fields.Pi);
+            Console.WriteLine(FieldClass.Name);
+        }
+    }
+}
