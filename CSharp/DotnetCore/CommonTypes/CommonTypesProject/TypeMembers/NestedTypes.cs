253d4c99c81a113b08195a615b325387ff09c7ed
sarai romero <sarairomero1025@hotmail.com>
2026-02-19 20:36:47 -0500

    Nested types test

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeMembers/NestedTypes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeMembers
+{
+    public class Container
+    {
+        public class Nested
+        {
+            public Container parent;
+
+            public Nested()
+            {
+                
+            }
+            public Nested(Container parent)
+            {
+                this.parent = parent;
+            }
+        }
+    }
+
+    public class ExampleNestedTypes
+    {
+        public void Method()
+        {
+            //var nestedError = new Nested(); To access to nested class is required access first to the container, not directly
+            var container = new Container();
+            var nest = new Container.Nested();
+        }
+    }
+}
