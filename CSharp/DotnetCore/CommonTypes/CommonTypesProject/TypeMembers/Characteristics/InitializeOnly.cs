406503dffcd6df4da21170396cdb96c32c0ab150
sarai romero <sarairomero1025@hotmail.com>
2026-02-22 20:18:35 -0500

    initialize only for properties

---
+++ CSharp/DotnetCore/CommonTypes/CommonTypesProject/TypeMembers/Characteristics/InitializeOnly.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeMembers.Characteristics
+{
+    internal class PersonInitializeOnly
+    {
+        #region Properties
+        public string Name { get; init; }
+        public int Age { get; set; }
+        public string Mail { get; } //implicit readonly 
+        #endregion
+        public class InitilizeOnly
+        {
+            public  void Method()
+            {
+                var person = new PersonInitializeOnly(){ Name = "A", Age = 1 };
+
+                //person.Mail = "a@gmail.com"; //Error: property is read only, it could never be change outside itself.
+                //person.Name = "B"; //Error: init only property, can only be assign (at initialization) but not modified
+                person.Age = 2;
+            }
+        }
+    }
+
+}
